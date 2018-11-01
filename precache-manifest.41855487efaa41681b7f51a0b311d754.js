354d540ebdf9e2a7e170dc945634cd8feb701752
DarkYamato <werewolfoftheshadow@gmail.com>
2018-11-01 16:22:08 +0300

    Updates

---
+++ precache-manifest.41855487efaa41681b7f51a0b311d754.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/admin-panel/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "0d2d819e4b78bca17673",
+    "url": "/admin-panel/static/css/main.d13c36b4.chunk.css"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/admin-panel/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "03a33348caf68f87543c",
+    "url": "/admin-panel/static/js/1.03a33348.chunk.js"
+  },
+  {
+    "revision": "6539c10dac0d85c5bbd7",
+    "url": "/admin-panel/static/js/runtime~main.6539c10d.js"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/admin-panel/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/admin-panel/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/admin-panel/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/admin-panel/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/admin-panel/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/admin-panel/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0d2d819e4b78bca17673",
+    "url": "/admin-panel/static/js/main.0d2d819e.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/admin-panel/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/admin-panel/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/admin-panel/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/admin-panel/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/admin-panel/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/admin-panel/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/admin-panel/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/admin-panel/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "03a33348caf68f87543c",
+    "url": "/admin-panel/static/css/1.3d7a13b9.chunk.css"
+  },
+  {
+    "revision": "1cdc0fc087d0d42c83aa0f3a3438edea",
+    "url": "/admin-panel/index.html"
+  }
+];
